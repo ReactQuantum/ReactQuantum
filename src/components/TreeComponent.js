0a5e1cb8b56dcacae6ade35f7577bf47623df764
Eric <echoy20@gmail.com>
2019-01-16 12:28:47 -0500

    commiting before a pull from dev

--- src/components/TreeComponent.js
+++ src/components/TreeComponent.js
@@ -1,46 +1,18 @@
 import React from 'react';
 import Tree from 'react-d3-tree';
 
-const myTreeData = [
-  {
-    name: 'Top Level',
-    attributes: {
-      keyA: 'val A',
-      keyB: 'val B',
-      keyC: 'val c'
-    },
-    stats: "SOMETHING!",
-    children: [
-      {
-        name: 'Level 2: A',
-        stats: '100ms',
-        nodeSvgShape: {
-          shape: 'rect',
-          shapeProps: {
-            width: 20,
-            height: 40,
-            x: -10,
-            y: -10,
-          }
-        },
 
-        attributes: {
-          keyA: 'val A',
-        },
-        children: [{name: 'Jen',stats: '101ms', separation: {siblings: 20, nonSiblings: 20}}, {name: 'john',stats: '102ms',},{name: 'Jen',stats: '103ms',}, {name: 'john',stats: '120ms',},{name: 'Jen',stats: '1111ms',}, {name: 'john',stats: '120ms',}]
-      },
-      {
-        name: 'Level 2: B',
-        children: [{name: 'Jen'}, {name: 'john'},{name: 'Jen'}, {name: 'john'},{name: 'Jen'}, {name: 'john'}]
-      },
-      {
-        name: 'Level 2: C',
-        attributes: {color: 'red'},
-        children: [{name: 'Jen'}, {name: 'john'},{name: 'Jen'}, {name: 'john'},{name: 'Jen'}, {name: 'john'}]
-      },
-    ],
-  },
-];
+
+const test2 = {
+  name: 'Level 2: C',
+  children: [{name: 'Level 3C: A', stats: '101ms'},
+  {name: 'Level 3C: B', stats: '102ms'},
+  {name: 'Level 3C: C', stats: '103ms'},
+  {name: 'Level 3C: D', stats: '120ms'},
+  {name: 'Level 3C: E', stats: '1111ms'}]
+};
+
+const myTreeData = test1;
 
 const svgSquare = {
   shape: 'rect',
@@ -80,11 +52,16 @@ class TreeComponent extends React.Component {
     this.props.grabNodeStats(info);
   }
 
+
+  componentWillUpdate () {
+    
+  }
+
   render() {
 
     return (
 
-      <div id="treeWrapper" style={{width: '100%', height: '20em'}}>
+      <div id="treeWrapper" style={{width: '100%', height: '50em'}}>
 
         <Tree orientation='vertical' onClick={this.handleOnClick} nodeSvgShape={this.state.shape} separation={{siblings: .3,nonSiblings: .3}} data={myTreeData} />
 
