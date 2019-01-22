1cfa29fb550d1b7d6e0eb8e14360d129998667b3
Woojae Park <wjp241@nyu.edu>
2019-01-21 20:41:13 -0500

    last child's sibling's child problem fixed

--- build/inject.js
+++ build/inject.js
@@ -17,15 +17,15 @@ function filter(fiber) {
     } else if (typeof elementType === "object") {
       name = elementType.displayName;
     }
-  } else {
-    if (stateNode) {
-      if (stateNode.containerInfo) {
-        name = stateNode.containerInfo.id
-      }
-      name = stateNode.nodeName
-    } else {
-      name = "Unknown"
+  } else if (stateNode) {
+    if (stateNode.containerInfo) {
+      name = stateNode.containerInfo.id
     }
+    name = stateNode.nodeName
+  }
+
+  if (name === undefined || name === "" || name === null) {
+    name = "Unknown"
   }
 
   let filteredFiber = {
@@ -87,17 +87,27 @@ function createChild(workInProgress) {
   }
   return bottomFiber;
 }
+
 function createTree(workInProgress) {
   // 1.next is current fiber's child
   let next = workInProgress.child
-
   let keepClimb = true;
   let keepCreateChild = true;
   while (keepCreateChild) {
     //2.pass in child fiber to createChild function
     let bottomFiber = createChild(next);
     let climber = bottomFiber;
+    let climberSib
+    //if bottomfiber has a child go back into the loop
+    if (climber.child !== null) {
+      climberSib = climber.child;
+    }
     while (keepClimb) {
+      if (climberSib !== undefined) {
+        targ = targ.sibling;
+        next = climberSib;
+        break;
+      }
       //if bottomFiber has a parent
       if (climber.return !== null) {
         //if the parent has a sibling
@@ -120,6 +130,7 @@ function createTree(workInProgress) {
       }
     }
   }
+  return arr
   //deleting circular references
   let noCirc = JSON.stringify(arr, function (key, val) {
     if (!Array.isArray(val) && val !== null && typeof val === "object") {
