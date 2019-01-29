fb5f1896fc540a3d4bb9bb2aa92527d74695546c
Altai <alchi1206@gmail.com>
2019-01-28 19:04:31 -0500

    es lint

---
+++ src/devtools.jsx
@@ -0,0 +1,217 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import styled from 'styled-components';
+import TreeComponent from './components/TreeComponent';
+import Button from './components/Button';
+import PercentColorInput from './components/PercentColorInput';
+import Stats from './components/Stats';
+import image from './assets/ReactQuantumLogo2.png';
+
+const WrapperStyled = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // orientation: 'vertical',
+      nodeinfo: 5,
+      startQuantum: false,
+      treeData: {
+        name: 'PLEASE REFRESH PAGE',
+        nodeSvgShape: { shapeProps: { fill: 'white' } },
+      },
+      green: 0.005,
+      lightGreen: 0.01,
+      yellow: 0.05,
+      orange: 0.1,
+    };
+
+    // this.changePercentages = this.changePercentages.bind(this);
+    this.updateTreeState = this.updateTreeState.bind(this);
+    this.grabNodeStats = this.grabNodeStats.bind(this);
+    // this.changeOrientation = this.changeOrientation.bind(this);
+    this.clicked = this.clicked.bind(this);
+    this.startQuantum = this.startQuantum.bind(this);
+    chrome.devtools.panels.create('React Quantum', null, 'devtools.html');
+  }
+
+  // changeOrientation() {
+  // if (this.state.orientation === 'vertical') {
+  // this.setState({ orientation: 'horizontal' })
+  //   } else {
+  //     this.setState({ orientation: 'vertical' })
+  //   }
+  // }
+
+  componentDidMount() {
+    const port = chrome.runtime.connect(null, { name: 'devTools' });
+    const { tabId } = chrome.devtools.inspectedWindow;
+
+    function post(message) {
+      message.tabId = tabId;
+      port.postMessage(message);
+    }
+    post({ message: 'initialize' });
+    port.onMessage.addListener((message) => {
+      // function subtracts children render time from its own render time to get individual render time
+      function addIndividualTime(treeDataArr) {
+        for (let i = 0; i < treeDataArr.length; i += 1) {
+          if (treeDataArr[i].memoizedProps) {
+            treeDataArr[i].memoizedProps = JSON.parse(treeDataArr[i].memoizedProps);
+          }
+          if (treeDataArr[i].renderTime === 0) {
+            treeDataArr[i].individualTime = 0;
+          } else {
+            let sumChildrenTime = 0;
+            for (let j = 0; j < treeDataArr[i].children.length; j += 1) {
+              const currentNode = treeDataArr[i].children[j];
+              if (currentNode.renderTime === 0 && currentNode.children > 0) {
+                for (let k = 0; k < currentNode.children.length; k += 1) {
+                  sumChildrenTime += currentNode.children[k].renderTime;
+                }
+              } else {
+                sumChildrenTime += treeDataArr[i].children[j].renderTime;
+              }
+            }
+            console.log('in addIndividualTime', treeDataArr[i].name, treeDataArr[i].renderTime, sumChildrenTime);
+            treeDataArr[i].individualTime = treeDataArr[i].renderTime - sumChildrenTime;
+          }
+        }
+        for (let i = 0; i < treeDataArr.length; i += 1) {
+          if (treeDataArr[i].children.length > 0) {
+            addIndividualTime(treeDataArr[i].children);
+          }
+        }
+      }
+
+      // adds color based on render time of node relative to total render time of app
+      function addColor(treeDataArr, green, lightGreen, yellow, orange) {
+        const totalTime = treeDataArr[0].renderTime;
+        const workToBeDone = [treeDataArr[0]];
+        while (workToBeDone.length > 0) {
+          const percentTime = workToBeDone[0].individualTime / totalTime;
+          if (percentTime < green) {
+            workToBeDone[0].nodeSvgShape = { shape: 'ellipse', shapeProps: { rx: 20, ry: 20, fill: '#80b74c' } };
+          } else if (percentTime < lightGreen) {
+            workToBeDone[0].nodeSvgShape = { shape: 'ellipse', shapeProps: { rx: 20, ry: 20, fill: '#a1c94f' } };
+          } else if (percentTime < yellow) {
+            workToBeDone[0].nodeSvgShape = { shape: 'ellipse', shapeProps: { rx: 20, ry: 20, fill: '#e6cc38' } };
+          } else if (percentTime < orange) {
+            workToBeDone[0].nodeSvgShape = { shape: 'ellipse', shapeProps: { rx: 20, ry: 20, fill: '#f69d27' } };
+          } else {
+            workToBeDone[0].nodeSvgShape = { shape: 'ellipse', shapeProps: { rx: 20, ry: 20, fill: '#e74e2c' } };
+          }
+          for (let i = 0; i < workToBeDone[0].children.length; i += 1) {
+            workToBeDone.push(workToBeDone[0].children[i]);
+          }
+          workToBeDone.shift();
+        }
+        return treeDataArr;
+      }
+
+      const {
+        green, lightGreen, yellow, orange,
+      } = this.state;
+
+      console.log('chrome.runtime.onMessage in devTools message:', message);
+      let tempTreeData = JSON.parse(message.message);
+      tempTreeData = tempTreeData[0].children;
+      console.log('before addIndividualTime', tempTreeData);
+      addIndividualTime(tempTreeData);
+      console.log('after individualTime =============', tempTreeData);
+      addColor(tempTreeData, green, lightGreen, yellow, orange);
+      console.log('after addColor =============', tempTreeData);
+      this.setState({ treeData: tempTreeData });
+      console.log('after setState', this.state);
+    });
+  }
+
+  startQuantum() {
+    const { tabId } = chrome.devtools.inspectedWindow;
+    chrome.runtime.sendMessage({
+      name: 'startQuantum',
+      target: 'content',
+      tabId,
+    });
+
+    this.setState({
+      startQuantum: true,
+    });
+  }
+
+  updateTreeState(treeDataArr) {
+    this.setState({ treeData: treeDataArr });
+  }
+
+  grabNodeStats(stats) {
+    this.setState({ nodeinfo: { totalTime: stats.time, individualTime: stats.individualTime, name: stats.name } });
+    console.log(stats);
+  }
+
+  clicked(e) {
+    const counterI = `${e.target.id}counter`;
+    const { counterId } = this.state;
+    const counter = counterId + 1;
+    const updateCounter = {};
+
+    updateCounter[counterI] = counter;
+    this.setState(updateCounter);
+  }
+
+  render() {
+    console.log('render ------------');
+    const {
+      nodeinfo, treeData, green, lightGreen, yellow, orange, orientation, startQuantum,
+    } = this.state;
+    return (
+      <WrapperStyled>
+        <div>
+          <img src={image} href="https://github.com/ReactQuantum/ReactQuantum" alt="" />
+
+
+          {startQuantum === false
+            ? (
+              <div style={{
+                width: '100%', alignContent: 'center', display: 'flex', justifyContent: 'center',
+              }}
+              >
+                <Button
+                  id="startQuantum"
+                  clicked={this.startQuantum}
+                  counter="Start Quantum"
+                />
+
+              </div>
+            )
+            : (
+              <div className="content">
+                <div>
+                  <Stats stats={nodeinfo} />
+                  <PercentColorInput
+                    treeData={treeData}
+                    percentForGreen={green}
+                    percentForLightGreen={lightGreen}
+                    percentForYellow={yellow}
+                    percentForOrange={orange}
+                    updateTreeState={this.updateTreeState}
+                  />
+                </div>
+                <TreeComponent
+                  orientation={orientation}
+                  treeData={treeData}
+                  grabNodeStats={this.grabNodeStats}
+                />
+              </div>
+            )
+          }
+        </div>
+      </WrapperStyled>
+
+    );
+  }
+}
+
+
+render(<App />, document.getElementById('root'));
