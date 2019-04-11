aa192c9e6a940652de2be059d14e9d53e68a99ac
Woojae Park <wjp241@nyu.edu>
2019-04-10 21:30:29 -0400

    actualDuration fixed

--- build/inject.js
+++ build/inject.js
@@ -1,85 +1,86 @@
 
 (() => {
 
-  const extractFiber = () => {
-    const hookedTree = Object.values(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._fiberRoots)[0];
-    let fiber
+    const extractFiber = () => {
+        const hookedTree = Object.values(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._fiberRoots)[0];
+        let fiber
 
-    for (const value of hookedTree.values()) {
-      fiber = value.current;
+        for (const value of hookedTree.values()) {
+            fiber = value.current;
+        }
+
+        return fiber;
     }
 
-    return fiber;
-  }
+    const filter = fiber => {
+        if (!fiber) return fiber;
 
-  const filter = fiber => {
-    if (!fiber) return fiber;
+        const { actualDuration, elementType, child, sibling } = fiber;
+        let name;
 
-    const { actualDuration, elementType, child, sibling } = fiber;
-    let name;
+        if (elementType !== null) {
+            if (typeof elementType === 'string') name = elementType;
+            if (typeof elementType === 'function') name = elementType.name;
+            if (typeof elementType === 'object') name = elementType.displayName;
+        }
 
-    if (elementType !== null) {
-      if (typeof elementType === 'string') name = elementType;
-      if (typeof elementType === 'function') name = elementType.name;
-      if (typeof elementType === 'object') name = elementType.displayName;
+        if (name === undefined || name === '' || name === null) name = 'Unknown';
+
+        const filteredFiber = {
+            name,
+            renderTime: actualDuration ? actualDuration : 'Only available in Dev Mode',
+            child,
+            sibling: filter(sibling)
+        };
+
+        console.log(filteredFiber.renderTime);
+        return filteredFiber;
     }
 
-    if (name === undefined || name === '' || name === null) name = 'Unknown';
+    const current = extractFiber();
+    const root = filter(current);
 
-    const filteredFiber = {
-      name,
-      renderTime: actualDuration ? 'Only available in Dev Mode' : actualDuration,
-      child,
-      sibling: filter(sibling)
-    };
+    const d3 = fiber => {
+        if (fiber.child === null) return fiber;
 
-    return filteredFiber;
-  }
+        const child = vertical(fiber);
+        const arr = horizontal(child);
+        fiber.child = arr;
+        let i = arr.length - 1;
 
-  const current = extractFiber();
-  const root = filter(current);
+        while (i >= 0) {
+            d3(arr[i]);
+            i--;
+        }
 
-  const d3 = fiber => {
-    if (fiber.child === null) return fiber;
-
-    const child = vertical(fiber);
-    const arr = horizontal(child);
-    fiber.child = arr;
-    let i = arr.length - 1;
-
-    while (i >= 0) {
-      d3(arr[i]);
-      i--;
+        return fiber;
     }
 
-    return fiber;
-  }
+    const vertical = fiber => {
+        let nthCh = fiber;
+        nthCh = fiber.child;
 
-  const vertical = fiber => {
-    let nthCh = fiber;
-    nthCh = fiber.child;
-
-    return nthCh;
-  }
-
-  const horizontal = fiber => {
-    let nthSib = filter(fiber);
-    const arr = [nthSib];
-
-    while (nthSib.sibling) {
-      nthSib = nthSib.sibling;
-      arr.push(nthSib);
+        return nthCh;
     }
 
-    return arr;
-  }
+    const horizontal = fiber => {
+        let nthSib = filter(fiber);
+        const arr = [nthSib];
 
-  d3(root);
+        while (nthSib.sibling) {
+            nthSib = nthSib.sibling;
+            arr.push(nthSib);
+        }
 
-  window.postMessage({
-    name: 'inject',
-    data: JSON.stringify(root),
-  });
+        return arr;
+    }
+
+    d3(root);
+
+    window.postMessage({
+        name: 'inject',
+        data: JSON.stringify(root),
+    });
 
 })()
 
