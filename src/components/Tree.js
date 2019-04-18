d5529c32f6850547d845d0e267fd244f5f83b580
Woojae Park <wjp241@nyu.edu>
2019-04-18 02:05:52 -0400

    libraries can't handle large data set fast enough, how to address this?

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,127 +1,199 @@
-import React, { Fragment, Component } from "react";
-import { Group } from "@vx/group";
-import { Tree } from "@vx/hierarchy";
-import { LinearGradient } from "@vx/gradient";
-import { hierarchy } from "d3-hierarchy";
-import Links from "./LinksMove";
-import Nodes from "./NodesMove";
+import React from 'react';
+import { Group } from '@vx/group';
+import { Tree } from '@vx/hierarchy';
+import { LinkHorizontal } from '@vx/shape';
+import { hierarchy } from 'd3-hierarchy';
+import { LinearGradient } from '@vx/gradient';
 
-export default class App extends Component {
-  state = {
-    layout: "cartesian",
-    orientation: "horizontal",
-    linkType: "diagonal",
-    stepPercent: 0.5
-  };
+const peach = '#fd9b93';
+const pink = '#fe6e9e';
+const blue = '#03c0dc';
+const green = '#26deb0';
+const plum = '#71248e';
+const lightpurple = '#374469';
+const white = '#ffffff';
+const bg = '#272b4d';
 
-  render() {
-    const {
-      data,
-      width,
-      height,
-      events = false,
-      margin = {
-        top: 30,
-        left: 30,
-        right: 30,
-        bottom: 30
-      }
-    } = this.props;
-    const { layout, orientation, linkType, stepPercent } = this.state;
+const tree = {
+  "name": "T",
+  "children": [{
+    "name": "A",
+    "children": [
+      { "name": "A1" },
+      { "name": "A2" },
+      { "name": "A3" },
+      {
+        "name": "C",
+        "children": [{
+          "name": "C1",
+        }, {
+          "name": "D",
+          "children": [{
+            "name": "D1"
+          }, {
+            "name": "D2"
+          }, {
+            "name": "D3"
+          }]
+        }]
+      },
+    ]
+  },
+  { "name": "Z" },
+  {
+    "name": "B",
+    "children": [
+      { "name": "B1" },
+      { "name": "B2" },
+      { "name": "B3" },
+    ]
+  },
+  ],
+};
 
-    if (width < 10) return null;
+function Node({ node }) {
+  const width = 40;
+  const height = 20;
+  const centerX = -width / 2;
+  const centerY = -height / 2;
+  const isRoot = node.depth === 0;
+  const isParent = !!node.children;
 
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
+  if (isRoot) return <RootNode node={node} />;
+  if (isParent) return <ParentNode node={node} />;
 
-    let origin;
-    let sizeWidth;
-    let sizeHeight;
-
-    if (layout === "polar") {
-      origin = {
-        x: innerWidth / 2,
-        y: innerHeight / 2
-      };
-      sizeWidth = 2 * Math.PI;
-      sizeHeight = Math.min(innerWidth, innerHeight) / 2;
-    } else {
-      origin = { x: 0, y: 0 };
-      if (orientation === "vertical") {
-        sizeWidth = innerWidth;
-        sizeHeight = innerHeight;
-      } else {
-        sizeWidth = innerHeight;
-        sizeHeight = innerWidth;
-      }
-    }
-
-    const root = hierarchy(data, d => (d.isExpanded ? d.children : null));
-
-    return (
-      <Fragment>
-        <div style={{ margin: "10px 0px" }}>
-          <span style={{ marginRight: 10 }}>
-            <label>orientation:</label>
-            <select
-              onChange={e => this.setState({ orientation: e.target.value })}
-              value={orientation}
-              disabled={layout === "polar"}
-            >
-              <option value="vertical">vertical</option>
-              <option value="horizontal">horizontal</option>
-            </select>
-          </span>
-          <span>
-            <label>link:</label>
-            <select
-              onChange={e => this.setState({ linkType: e.target.value })}
-              value={linkType}
-            >
-              <option value="diagonal">diagonal</option>
-              <option value="step">step</option>
-              <option value="line">line</option>
-            </select>
-          </span>
-        </div>
-        <svg width={width} height={height}>
-          <LinearGradient id="lg" from="#fff" to="#aaa" />
-          <rect width={width} height={height} rx={14} fill="#888" />
-          <Tree
-            top={margin.top}
-            left={margin.left}
-            root={root}
-            size={[sizeWidth, sizeHeight]}
-            separation={(a, b) => (a.parent == b.parent ? 1 : 0.5) / a.depth}
-          >
-            {({ data }) => (
-              <Group top={origin.y} left={origin.x}>
-                <Links
-                  links={data.links()}
-                  linkType={linkType}
-                  layout={layout}
-                  orientation={orientation}
-                  stepPercent={stepPercent}
-                />
-
-                <Nodes
-                  nodes={data.descendants()}
-                  layout={layout}
-                  orientation={orientation}
-                  onNodeClick={node => {
-                    if (!node.data.isExpanded) {
-                      node.data.x0 = node.x;
-                      node.data.y0 = node.y;
-                    }
-                    node.data.isExpanded = !node.data.isExpanded;
-                    this.forceUpdate();
-                  }}
-                />
-              </Group>
-            )}
-          </Tree>
-        </svg>
-      </Fragment>
-    );
-  }
+  return (
+    <Group top={node.x} left={node.y}>
+      <rect
+        height={height}
+        width={width}
+        y={centerY}
+        x={centerX}
+        fill={bg}
+        stroke={green}
+        strokeWidth={1}
+        strokeDasharray={'2,2'}
+        strokeOpacity={0.6}
+        rx={10}
+        onClick={() => {
+          alert(`clicked: ${JSON.stringify(node.data.name)}`);
+        }}
+      />
+      <text
+        dy={'.33em'}
+        fontSize={9}
+        fontFamily="Arial"
+        textAnchor={'middle'}
+        fill={green}
+        style={{ pointerEvents: 'none' }}
+      >
+        {node.data.name}
+      </text>
+    </Group>
+  );
 }
+
+function RootNode({ node }) {
+  return (
+    <Group top={node.x} left={node.y}>
+      <circle r={12} fill="url('#lg')" />
+      <text
+        dy={'.33em'}
+        fontSize={9}
+        fontFamily="Arial"
+        textAnchor={'middle'}
+        style={{ pointerEvents: 'none' }}
+        fill={plum}
+      >
+        {node.data.name}
+      </text>
+    </Group>
+  );
+}
+
+function ParentNode({ node }) {
+  const width = 40;
+  const height = 20;
+  const centerX = -width / 2;
+  const centerY = -height / 2;
+
+  return (
+    <Group top={node.x} left={node.y}>
+      <rect
+        height={height}
+        width={width}
+        y={centerY}
+        x={centerX}
+        fill={bg}
+        stroke={blue}
+        strokeWidth={1}
+        onClick={() => {
+          alert(`clicked: ${JSON.stringify(node.data.name)}`);
+        }}
+      />
+      <text
+        dy={'.33em'}
+        fontSize={9}
+        fontFamily="Arial"
+        textAnchor={'middle'}
+        style={{ pointerEvents: 'none' }}
+        fill={white}
+      >
+        {node.data.name}
+      </text>
+    </Group>
+  );
+}
+
+const fuck = ({ treeData,
+  width,
+  height,
+  margin = {
+    top: 10,
+    left: 30,
+    right: 40,
+    bottom: 80
+  }
+}) => {
+  console.log(treeData)
+  let data
+  if (!treeData) {
+    data = hierarchy(tree);
+  } else {
+    data = hierarchy(treeData);
+  }
+  const yMax = height - margin.top - margin.bottom;
+  const xMax = width - margin.left - margin.right;
+
+  return (
+    <svg width={width} height={height}>
+      <LinearGradient id="lg" from={peach} to={pink} />
+      <rect width={width} height={height} rx={14} fill={bg} />
+      <Tree root={data} size={[yMax, xMax]}>
+        {tree => {
+          return (
+            <Group top={margin.top} left={margin.left}>
+              {tree.links().map((link, i) => {
+                return (
+                  <LinkHorizontal
+                    key={`link-${i}`}
+                    data={link}
+                    stroke={lightpurple}
+                    strokeWidth="1"
+                    fill="none"
+                  />
+                );
+              })}
+              {tree.descendants().map((node, i) => {
+                return <Node key={`node-${i}`} node={node} />;
+              })}
+            </Group>
+          );
+        }}
+      </Tree>
+    </svg>
+  );
+
+}
+
+export default fuck
